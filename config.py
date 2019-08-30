8ef617bcca1dd9eb0c6b768cc375d596a53abaa6
Donovan <donovan.bidlack@gmail.com>
2019-08-30 14:12:53 -0600

    Merged

---
+++ config.py
@@ -0,0 +1,14 @@
+alpaca_api_secret_key = "nu3XZr6PZqF7RoktKS4sWrYYJlcUx5hV80nzV126"
+alpaca_api_key_id = "PKK7DQITNPPXY58HVJ2S"
+IEX_api_secret_key = "pk_11551eefe1bf4f0b81121b498c6a7651"
+
+tweepy_consumer_key = "9TxQyYHvAujwVOGNW5Di97lsL"
+tweepy_consumer_secret = "3TX9wmny6Rt1KQ5zwfCbta7X1L1Zw7rJJiuV44rngVarupdQAt"
+tweepy_access_token = "1103715647127613441-h28XoZXcKIOSxP18U2vnqlJNGqPNao"
+tweepy_access_token_secret = "dNtpJ5825K070HBSFOCnD3CayTgm5DmvZmHrWZw2DgYkS"
+
+newsapi_api_key_id = "36fc08386f85499c93487f0c03efba50"
+
+alphaadvantage_api_key_id = "WRV0ICYRFLQOX96V"
+
+indico_api_key_id = "d6bb482f0d1165b6912b2816246b8ffb"
